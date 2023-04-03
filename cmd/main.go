4b57af8c7d70794fc5ffeff808a9a131c55aaf6c
Rakhatbek <r.arinov@kazdream.kz>
2023-04-03 20:57:31 +0600

    added migrations

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"awesomeTodoApp/pkg/handler"
 	"awesomeTodoApp/pkg/repository"
 	"awesomeTodoApp/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init configs")
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	err := srv.Run("8000", handlers.InitRoutes())
+	err := srv.Run(viper.GetString("port"), handlers.InitRoutes())
 	if err != nil {
 		log.Fatalf("[main]: Error occrured while runnig http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
